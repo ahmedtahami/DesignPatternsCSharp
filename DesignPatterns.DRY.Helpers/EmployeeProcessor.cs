a28dabeb34791a79a1393b7f8ae0a98bb8f9ac12
Anaeem <ahmed.naeem@ascendbpo.us>
2022-04-16 00:12:55 +0500

    [Added] seprate Class Library for SOC

---
+++ DesignPatterns.DRY.Helpers/EmployeeProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Helpers
+{
+    public record EmployeeProcessor
+    {
+        public string EmployeeId { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public EmployeeProcessor(string firstName, string lastName)
+        {
+            try
+            {
+                if (String.IsNullOrWhiteSpace(firstName) || String.IsNullOrWhiteSpace(lastName))
+                    throw new Exception("Validation Error!");
+                this.EmployeeId = GenerateEmployeeId(firstName, lastName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static string GenerateEmployeeId(string firstName, string lastName)
+        {
+            return $@"{firstName?[..(firstName.Length/2)]}{lastName[..(lastName.Length / 2)]}{DateTime.Now.Ticks.ToString().Substring(0, 4)}";
+        }
+    }
+}
--- DesignPattrens/Program.cs
+++ DesignPattrens/Program.cs
@@ -1,18 +1,19 @@
-﻿class Program
+﻿using DesignPatterns.Helpers;
+
+class Program
 {
     static void Main(string[] args)
     {
-        string FirstName = "John", LastName = "Kenedy";
-        //var employeeId = $@"{FirstName[..4]}{LastName[..4]}
-        //                    {DateTime.Now.Ticks.ToString().Substring(0, 4)}";
-        var employeeId = GenerateEmployeeId(FirstName,LastName);
-        Console.WriteLine("Your Employee Id is : {0}", employeeId);
-        
-    }
-    // DRY - DON'T REPEAT YOURSELF
-    // Created a discrete and independant method
-    private static string GenerateEmployeeId(string FirstName, string LastName)
-    {
-        return $@"{FirstName[..4]}{LastName[..4]}{DateTime.Now.Ticks.ToString().Substring(0, 4)}";
+        try
+        {
+            string FirstName = "", LastName = "Keenedy";
+            EmployeeProcessor employeeProcessor = new EmployeeProcessor(FirstName, LastName);
+            if (!String.IsNullOrWhiteSpace(employeeProcessor.EmployeeId))
+                Console.WriteLine("Your Employee Id is : {0}", employeeProcessor.EmployeeId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
